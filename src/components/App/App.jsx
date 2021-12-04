b51ef8d6cb6593fea9dcaa61207eae921922f477
emanuel caro <darkemlord@gmail.com>
2021-12-04 17:21:45 +0900

    app started, two components created

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+function App() {
+  return (
+    <>
+      <BusinessList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BusinessList/BusinessList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Business from '../Business/Business'
+import '../BusinessList/BusinessList.css'
+
+const BusinessList = () => {
+  return (
+    <div className="BusinessList">
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+    </div>
+  )
+}
+
+export default BusinessList;
